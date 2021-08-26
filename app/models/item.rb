69ebfa1c258bec7a22bf326f669f71bef7613cf0
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-08-26 19:43:36 +0900

    バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :imege
+
+  with_options presence: true do
+    validates :item_name
+    validates :info
+    validates :category_id
+    validates :sipping_cost_id
+    validates :prefectures_id
+    validates :sipping_day_id
+    validates :price
+    validates :imege
+  end
 end
