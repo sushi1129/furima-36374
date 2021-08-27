67994452b9e09788097a53b00d81791d1a1f741a
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-08-27 14:15:11 +0900

    値段の範囲設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :item_name, :info, :image
-    validates :price
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
     with_options numericality: { other_than: 0 , message: "can't be blank"} do
       validates :category_id,    :condition_id,  :sipping_cost_id, 
