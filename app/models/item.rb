1759139706dc3b7726510d308a9aab09cc0b3fb7
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-08-27 15:57:25 +0900

    値段設定範囲の修正、エラーメッセージ追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,9 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :item_name, :info, :image
-    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    validates :price, inclusion: { in: 300..9999999, message: 'is out of setting range'},
+                      format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' },
+                      numericality: true
 
     with_options numericality: { other_than: 0 , message: "can't be blank"} do
       validates :category_id,    :condition_id,  :sipping_cost_id, 
