f738dc81b0ac58bfcb7ea8367a802e9f92b7ebc0
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-08-29 19:45:49 +0900

    削除アクション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -35,7 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-
+    @item.destroy
+    redirect_to root_path
   end
 
   private
