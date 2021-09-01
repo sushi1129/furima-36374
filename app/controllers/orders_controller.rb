716b44680010e11c59b55a46d4b08709eb8554c4
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-01 16:05:16 +0900

    ビューの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+  end
 end
