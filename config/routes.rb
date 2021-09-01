4648d9c9be4da6daee6fd694e100a9efb3df9e20
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-01 12:51:49 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
 
-  resources :items
+  resources :items do
+    resources :order, only: [:index, :create]
+  end
+
 end
