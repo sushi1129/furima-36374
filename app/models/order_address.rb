89d135cfddb10a2ce47d34dd14a683b758b70a75
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-01 20:25:22 +0900

    バリデーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,7 +5,8 @@ class OrderAddress
   with_options presence: true do
     validates :user_id, :item_id, :city, :house_number, :order_id
     validates :postal_cade, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
-    validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
+    validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"},
+                                 length: {minimum: 10, message: "is too short"}
   end
   validates :prefectures_id, numericality: {other_than: 0, message: "can't be blank"}
 
