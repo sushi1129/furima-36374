0e76fc251dc45f351d27e2f7886e8e29ce84da9d
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-02 12:21:21 +0900

    データ保存が上手く出来ないエラー修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_cade, :prefectures_id, :city, :house_number, :building_name, :telephone_number, :order_id
+  attr_accessor :user_id, :item_id, :postal_cade, :prefectures_id, :city, :house_number, :building_name, :telephone_number
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :house_number, :order_id
+    validates :user_id, :item_id, :city, :house_number
     validates :postal_cade, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"},
                                  length: {minimum: 10, message: "is too short"}
