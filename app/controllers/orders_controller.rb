1a04666183f5eaad2477197134dbe104f2ff19dc
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-02 18:44:10 +0900

    クレジットカード機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -20,7 +21,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_cade, :prefectures_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_cade, :prefectures_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
@@ -31,5 +32,14 @@ class OrdersController < ApplicationController
     if @item.order.present? || current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_cade, :prefectures_id, :city, :house_number, :building_name, :telephone_number
+  attr_accessor :user_id, :item_id, :postal_cade, :prefectures_id, :city, :house_number, :building_name, :telephone_number, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :city, :house_number
+    validates :user_id, :item_id, :city, :house_number, :token
     validates :postal_cade, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only half-width number"},
                                  length: {minimum: 10, message: "is too short"}
