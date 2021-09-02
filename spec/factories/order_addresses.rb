2fcf2a44ef8c471ff03ff8c3aec05e8c1f1c15d3
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-02 18:51:23 +0900

    クレジットカード機能テスト実施

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     house_number     {'1-2-34'}
     building_name    {'ライオンズマンション'}
     telephone_number {12345678901}
+    token            {"tok_abcdefghijk00000000000000000"}
   end
 end
