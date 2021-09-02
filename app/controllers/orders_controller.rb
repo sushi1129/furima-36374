5348849c7b81249fc51e3a31d935bd80930171c4
sushi1129 <88273491+sushi1129@users.noreply.github.com>
2021-09-02 16:18:43 +0900

    購入ページ遷移条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
-  before_action :set_item, onry: [:index, :create]
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :status_verification, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -24,5 +25,11 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def status_verification
+    if @item.order.present? || current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
